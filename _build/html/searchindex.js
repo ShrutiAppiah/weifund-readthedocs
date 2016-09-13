034eb1077b480475606d390d75d88f276077318c
Shruti Appiah <shruti.appiah@gmail.com>
2016-09-13 11:31:44 -0400

    g

--- _build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,get:0,index:0,introduct:0,modul:0,page:0,search:0,start:0},titles:["Welcome to weifund-readthedocs&#8217;s documentation!"],titleterms:{document:0,indice:0,readthedoc:0,tabl:0,weifund:0,welcom:0}})
